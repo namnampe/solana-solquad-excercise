743b2ed7c2ef517a6b168cb8654f89451827fd47
namnampe <166782667+namnampe@users.noreply.github.com>
2024-04-12 11:48:33 +0700

    Update lib.rs

--- programs/solquad/src/lib.rs
+++ programs/solquad/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("5sFUqUTjAMJARrEafMX8f4J1LagdUQ9Y8TR8HwGNHkU8");
+declare_id!("Han7iw5fa4MmEMD3NL6sqZZ2QR8xSvdDJqwLwG3apPs");
 
 #[program]
 pub mod solquad {
